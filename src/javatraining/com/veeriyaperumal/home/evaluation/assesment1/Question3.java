9a2e9d40c95e922ec2cc966cba89dc59cddb91cd
veeriyaperumal <148101491+veeriyaperumal7@users.noreply.github.com>
2023-11-11 00:19:58 +0530

    Master (#6)
    * application
    
    * Console design changed
    
    * Bug fix scanner.close() and console design change
    
    * Merge branch 'master' of
    https://github.com/veeriyaperumal7/zsgstraining.git
    
    * Adding new programs and update in home class for terminate choice
    
    * demo push
    
    * Add new files

---
+++ src/javatraining/com/veeriyaperumal/home/evaluation/assesment1/Question3.java
@@ -0,0 +1,57 @@
+package javatraining.com.veeriyaperumal.home.evaluation.assesment1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Question3 {
+
+	public static void myMain() {
+		String arr[] = null;
+		main(arr);
+	}
+	
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		String  equation=read.nextLine();
+        if(isValidEquation(equation) && isValidParenthesis(equation)) {
+        	System.out.print("Valid");
+        }else {
+        	System.out.print("Invalid");
+        }
+	}
+
+	public static boolean isValidParenthesis(String equation) {
+		Stack<Character> stack = new Stack<>();
+		for(int i=0;i<equation.length();i++) {
+			if(equation.charAt(i)=='(') {
+				stack.push(equation.charAt(i));
+			}else if(equation.charAt(i)==')') {
+				if(stack.isEmpty()) {
+					return false;
+				}else {
+					stack.pop();
+				}
+			}
+		}
+		return (stack.isEmpty())?true : false;
+	}
+
+	public static boolean isValidEquation(String equation) {
+		equation=equation.toLowerCase();
+		for(int i=0;i<equation.length();i++) {
+			char c=equation.charAt(i);
+			if(c=='+'||c=='-'||c=='/'||c=='%'||c=='*') {
+				if(i-1<0 || i+1>=equation.length()) {
+					return false;
+				}else if(equation.charAt(i-1)==')' && equation.charAt(i+1)=='(') {
+					 continue;
+				}else if(equation.charAt(i-1)<'a' || equation.charAt(i-1)>'z' ||
+						equation.charAt(i+1)<'a' || equation.charAt(i+1)>'z' )  {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ src/javatraining/com/veeriyaperumal/home/evaluation/assesment1/Question5.java
@@ -0,0 +1,37 @@
+package javatraining.com.veeriyaperumal.home.evaluation.assesment1;
+import java.util.*;
+public class Question5 {
+	
+	public static void myMain() {
+		String arr[] = null;
+		main(arr);
+	}
+	
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter the number of strings to be entered :");
+        int n = read.nextInt();
+        read.nextLine();
+        String[] arr = new String[n];
+        for(int i=0;i<n;i++) {
+        	System.out.println("Enter the string"+(i+1));
+        	arr[i]=read.nextLine();
+        }
+        for(int i=0;i<n;i++) {
+        	System.out.println("String"+(i+1)+": "+makeLargestPossible(arr[i]));
+        	
+        }
+	}
+	public static String makeLargestPossible(String s) {
+		char[] arr = s.toCharArray();
+		s="";
+		Arrays.sort(arr);
+		for(int i=arr.length-1;i>=0;i--) {
+        	s+=arr[i];
+        }
+		return s;
+	}
+
+}

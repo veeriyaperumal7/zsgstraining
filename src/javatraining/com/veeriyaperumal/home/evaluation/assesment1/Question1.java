aa0b4bef93fbcfb217f5c76454875d45f80e216c
veeriyaperumal7 <148101491+veeriyaperumal7@users.noreply.github.com>
2023-11-24 00:29:13 +0530

    Fix bug in corner cases

--- src/javatraining/com/veeriyaperumal/home/evaluation/assesment1/Question1.java
+++ src/javatraining/com/veeriyaperumal/home/evaluation/assesment1/Question1.java
@@ -4,44 +4,46 @@ import java.util.Scanner;
 
 public class Question1 {
 
-	public static void myMain() {
-		String arr[] = null; 
-		main(arr);
-	}
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		System.out.print("Enter the number : ");
-	    int number = read.nextInt();
-	    int[][] pattern= new int[number][number];
-	    fillPattern(pattern,number);
-	    for(int[] arr : pattern) {
-	    	for(int i : arr) {
-	    		if(i!=0) {
-	    			if(i>9) {
-	    				System.out.print(i+" ");
-	    			}else {
-	    			System.out.print(i+"  ");
-	    			}
-	    	} else{
-	    		System.out.print("   ");
-	    	}
-	    	}System.out.println();
-	      }
+		int number = read.nextInt();
+		int[][] pattern = new int[number][number];
+		fillPattern(pattern, number);
+		for (int[] arr : pattern) {
+			for (int i : arr) {
+				if (i != 0) {
+					if (i > 9) {
+						System.out.print(i + " ");
+					} else {
+						System.out.print(i + "  ");
+					}
+				} else {
+					System.out.print("   ");
+				}
+			}
+			System.out.println();
 		}
+	}
 
 	private static void fillPattern(int[][] pattern, int number) {
-			int row=number-1,startRow=0,startColumn=0,endColumn=number-1,num=1;
-			while(startRow<=row || startColumn<=endColumn) {
-			for(int i=0;i<=row;i++) {
-				pattern[i][startColumn+i]=num++;
-			}startColumn++;
-			 row--;
-			for(int i=row;i>=startRow;i--) {
-				pattern[i][endColumn]=num++;
-			}endColumn--;
-			for(int i=endColumn;i>startColumn;i--) {
-				pattern[startRow][i]=num++;
-			}startRow++;row--;
+		int startRow = 0, endRow = number - 1, startColumn = 0, endColumn = number - 1, num = 1, rotationCounter = 0;
+		while (startRow <= endRow || startColumn <= endColumn) {
+			for (int i = startRow; i <= endRow; i++) {
+				pattern[i][startColumn + i] = num++;
+			}
+			startColumn++;
+			endRow--;
+			for (int i = endRow; i > startRow; i--) {
+				pattern[i][endColumn] = num++;
+			}
+			for (int i = endColumn; i >= startColumn + rotationCounter; i--) {
+				pattern[startRow][i] = num++;
+			}
+			endColumn--;
+			startRow++;
+			endRow--;
+			rotationCounter++;// Rotation counter used to avoid overlapping one previous values.
 		}
 	}
 }
